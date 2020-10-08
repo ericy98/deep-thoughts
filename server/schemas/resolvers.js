a9e1d4bc2ea6791f9ccc0732cd53feca0f97d0a2
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-08 16:41:53 -0500

    add thought, reaction, friend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -61,6 +61,49 @@ const resolvers = {
 
             const token = signToken(user);
             return { token, user };
+        },
+        addThought: async (parent, args, context) => {
+
+            // only logged in users
+            if (context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { thoughts: thought._id } },
+                    { new: true } // mongo returns updated document
+                );
+
+                return thought;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+            if (context.user) {
+                const updatedThought = await Thought.findOneAndUpdate(
+                    { _id: thoughtId },
+                    { $push: { reactions: { reactionBody, username: context.user.username } } },
+                    { new: true, runValidators: true }
+                );
+
+                return updatedThought
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        addFriend: async (parent, { friendId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { friends: friendId } },
+                    { new: true }
+                ).populate('friends');
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
