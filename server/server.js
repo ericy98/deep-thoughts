b4563e2490a319a345d64a4378c74428c650606c
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-08 16:15:55 -0500

    impliment auth token

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -10,8 +11,10 @@ const app = express();
 // know what API looks like, and how resolves requests
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
+
 
 server.applyMiddleware({ app });
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,6 +4,33 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+    authMiddleware: function({ req }) {
+        // token sent to 
+        let token = req.body.token  || req.query.token || req.headers.authorization;
+
+        // seperate "bearer" from "<tokenvalue>"
+        if (req.headers.authorization) {
+            token = token  
+                .split(' ')
+                .pop()
+                .trim();
+        }
+
+        // return as is
+        if(!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token')
+        }
+
+        // return updated request object
+        return req;
+    },
     signToken: function({ username, email, _id }) {
         const payload = { username, email, _id };
 
