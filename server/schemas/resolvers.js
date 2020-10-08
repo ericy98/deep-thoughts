5599d9213eda4dcaabbf93aec4bac28b4b87dc3f
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-07 19:52:26 -0500

    create user resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,10 +2,29 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
+
+        // GET all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password') // omit users passwords
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // GET a user by name
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password') 
+                .populate('friends')
+                .populate('thoughts');
+        },
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 }); // filter thru descending order
+        },
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
         }
+
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,14 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+            _id: ID
+            username: String
+            email: String
+            friendCount: Int
+            thoughts: [Thought]
+            friends: [User]
+        }
 
     type Thought {
         _id: ID
@@ -19,7 +27,10 @@ const typeDefs = gql`
     }
 
     type Query{
+        users: [User]
+        user(username: String!): User
         thoughts(username:String): [Thought]
+        thought(_id: ID!): Thought
     }    
 `;
 
